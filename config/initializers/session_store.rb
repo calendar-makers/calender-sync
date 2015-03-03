04be7dbefc6f73e07e510cf5fad8c491c02f0d46
Michael Soohoo <mbs729@gmail.com>
2015-03-03 13:26:06 -0800

    Fixed up issues regarding latest Ruby and Rails.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-CalendarSync::Application.config.session_store :cookie_store, key: '_calendar-sync_session'
+Rails.application.config.session_store :cookie_store, key: '_calendar-sync_session'
