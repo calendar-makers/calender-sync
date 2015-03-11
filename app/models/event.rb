df763115784dda677a5abda7744628863c06010b
Michael Soohoo <mbs729@gmail.com>
2015-03-10 19:53:10 -0700

    Reverted back to Mike's way, with better names.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ActiveRecord::Base
   #:name, :organization, :description, :date, :time, :location
+
+  def formatted_date
+    date.strftime('%B %d, %Y')
+  end
+
+  def formatted_time
+    time.strftime('%l:%M%P')
+  end
 end
