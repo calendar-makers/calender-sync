f37238c659afc4d05ab95ee5d55c3b2f3ea574ff
Michael Soohoo <mbs729@gmail.com>
2015-03-17 23:12:53 -0700

    Prepared step definitions for rsvp and images.

---
+++ features/step_definitions/images_steps.rb
@@ -0,0 +1,8 @@
+Then(/^I should see all the pictures for "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see any pictures for "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
---
+++ features/step_definitions/rsvp_steps.rb
@@ -0,0 +1,54 @@
+Given(/^the following guests exist:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the following registrations exist:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see info about people attending "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see info about people only attending "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the list of attendees should be listed alphabetically by "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the RSVP form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill out the RSVP form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a message confirming my submission$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see my information on the page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I do not fill out the entire RSVP form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a failed submission message$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see my information on the page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
