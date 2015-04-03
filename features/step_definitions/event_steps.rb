da781f7fbeb40e39e5c0066be22b7e1d52137066
Michael Soohoo <mbs729@gmail.com>
2015-04-02 21:00:35 -0700

    cleared merge conflict

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -1,16 +1,11 @@
 require_relative 'helper_steps'
 
-<<<<<<< HEAD
 Given /^the following events (?:exist:|exist on the calendar:)$/ do |events_table|
-=======
-Given /the following events exist/ do |events_table|
->>>>>>> 3a3b7d8334a271907db884dd25e901be52f884e8
   events_table.hashes.each do |event|
     Event.create!(event)
   end
 end
 
-<<<<<<< HEAD
 Given /I should see the following fields: "(.*)"$/ do |fields|
   fields.split(',').each do |field|
     step %Q{I should see the "#{field.strip}" on the page}
@@ -26,24 +21,6 @@ Then /I should see the "(.*)" on the page$/ do |field|
 end
 
 And /^I fill in the "(.*)" field with "(.*)"$/ do |field, value|
-=======
-Given /(?:|I )should see the following fields: "(.*)"$/ do |fields|
-  fields.split(', ').each do |field|
-    field = field.rstrip
-    expect(page).to have_content(field)
-  end
-end
-
-Then /(?:|I )should see the "(.*)" button$/ do |button_name|
-  expect(page).to have_button(button_name)
-end
-
-Then /(?:|I )should see the "(.*)" on the page$/ do |field|
-  expect(page).to have_content(field)
-end
-
-And /^(?:|I )fill in the "(.*)" field with "(.*)"$/ do |field, value|
->>>>>>> 3a3b7d8334a271907db884dd25e901be52f884e8
   field = find_field(field)
   field.set value
 end
@@ -52,16 +29,11 @@ And /^I select "([^"]*)" as the date and time$/ do |value|
   dt  = DateTime.strptime(value, "%m/%d/%Y, %H:%M%p")
   select dt.year, :from => 'event_start_1i'
   select dt.strftime("%B"), :from => 'event_start_2i'
-<<<<<<< HEAD
   select dt.day, :from => 'event_start_3i'
-=======
-  select dt.day, :from => 'event_start_3i' 
->>>>>>> 3a3b7d8334a271907db884dd25e901be52f884e8
   select dt.hour, :from => 'event_start_4i'
   select dt.min, :from => 'event_start_5i'
 end
 
-<<<<<<< HEAD
 Then /^I should see "(.*)" on "(.*)"$/ do |event_link, date|
   pending
   #expect(page).to have_link(event_link)
@@ -88,38 +60,6 @@ end
 
 Then /^I should see (?:the flash message |the message )?"([^"]*)"$/ do |message|
   expect(page).to have_content(message)
-=======
-Then /(?:|I )should see "(.*)" link on "(.*)"$/ do |event_link, date|
-  expect(page).to have_link(event_link)
-  ## NEEDS MORE WORK TO CHECK THAT THE LINK IS ACTUALLY UNDER THE GIVEN DATE
-  ## OR MAYBE SHOULD BE CHECKED SOMEWHERE ELSE???
-end
-
-=begin
-Then /(?:|I )should see "(.*)"$/ do |value|
-  #Very poorly made test, avoid using 
-  pending
-  #possible failures if duplicates exist.
-  #expect(page).to have_content(value)
-end
-=end
-
-Then /(?:|I )should see the "(.*)" link$/ do |link|
-  expect(page).to have_link(link)
-end
-
-Then /(?:|I )should not see the "(.*)" link$/ do |link|
-  expect(page).to_not have_link(link)
-end
-=begin
-Then /(?:|I )should not see "(.*)"$/ do |value|
-  expect(page).to_not have_content(value)
-end
-=end
-
-Then /(?:|I )should see the flash message "(.*)"$/ do |message|
-  @message == message
->>>>>>> 3a3b7d8334a271907db884dd25e901be52f884e8
 end
 
 Then /the "(.*)" field should be populated with "(.*)"$/ do |field, value|
@@ -140,13 +80,6 @@ Then /the "(.*)" field should not be populated$/ do |field|
   expect(field.labeled(field).value).to eq("")
 end
 
-<<<<<<< HEAD
-=======
-Given /^that I am logged in as "(.*)"$/ do |user_type|
-  pending
-end
-
->>>>>>> 3a3b7d8334a271907db884dd25e901be52f884e8
 Then /^I should not see the "(.*)" event$/ do |event_name|
   #pending based on calendar population, use "I should not see" for now
   pending
@@ -160,7 +93,6 @@ Then(/^I should see calendar navigation tools$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-<<<<<<< HEAD
 Given /^I am on the "(.*)" page(?: for "(.*)")?$/ do |page_name, event_name|
   if event_name
     visit path_to_event(page_name, event_name)
@@ -184,39 +116,6 @@ Then /^I should be on the "(.*)" page(?: for "(.*)")?$/ do |page_name, event_nam
   else
     expect(path_to(page_name)).to eq(current_path)
   end
-=======
-Given /^(?:|I )am on the "(.*)" page for "(.*)"$/ do |page_name, event_name|
-  visit path_to_event(page_name, event_name)
-end
-
-Given /^(?:|I )am on the "(.*)" page$/ do |page_name|
-  visit path_to(page_name)
-end
-
-When /^(?:|I )click on the "(.*)" link$/ do |link|
-  click_link(link)
-end
-
-When /^(?:|I )click on the "(.*)" button$/ do |button|
-  click_button(button)
-end
-
-Then /^(?:|I )should see "(.*)" as the "(.*)"$/ do |value, field|
-  pending
-  field = field.downcase
-  field = find_by_id(field)
-  expect(field).to have_content(value)
-end
-
-Then /^(?:|I )should be on the "(.*)" page for "(.*)"$/ do |page_name, event_name|
-  current_path = URI.parse(current_url).path
-  expect(path_to_event(page_name, event_name)).to eq(current_path)
-end
-
-Then /^(?:|I )should be on the "(.*)" page$/ do |page_name|
-  current_path = URI.parse(current_url).path
-  expect(path_to(page_name)).to eq(current_path)
->>>>>>> 3a3b7d8334a271907db884dd25e901be52f884e8
 end
 
 Given(/^"(.*)" exists$/) do |arg|
