81c6596a788c4fd9299ebc236171f134801368bc
vincent <vincehayashi@yahoo.com>
2015-04-03 22:17:22 -0700

    minor edits for coverage

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Guest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   describe "parse a name pulled from meetup event rsvp" do
     context "with a name including both first and last" do
       let(:good_name) {'Chester Copperpot'}
