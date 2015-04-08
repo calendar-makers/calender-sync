02330581b8c061b81f775fd7a737b8ff8a9d06ca
Calvin Yau <calvinyau@berkeley.edu>
2015-04-07 22:16:58 -0700

    cannot find src

--- features/step_definitions/images_steps.rb
+++ features/step_definitions/images_steps.rb
@@ -1,17 +1,18 @@
-Then(/^I should see all the pictures for "(.*)"$/) do |event_name|
-  pending
+When(/^I upload an image$/) do 
+  attach_file('image', File.join(Rails.root, '/features/upload_Files/nature1.jpg'), :visible => false)
+  click_button "Upload"
+end
+
+Then(/^I should see the picture "(.*)" for "(.*)"$/) do |image_name, event_name|
   id_num = Event.find_by_name(event_name).id
-  Dir.foreach("public/assets/#{id_num}/medium") do |item|
-    next if item == '.' or item == '..'
-    expect(page).to have_xpath("//img[@src=\"/assets/images/#{event_name}/#{item}\"]")
-  end
+  puts image_name
+  expect(page).to have_xpath("//img[@src=\"/public/assets/#{id_num}/medium/#{image_name}\"]")
 end
 
 Then(/^I should not see any pictures for "(.*)"$/) do |event_name|
-  pending
   id_num = Event.find_by_name(event_name).id
   Dir.foreach("public/assets/#{id_num}/medium") do |item|
     next if item == '.' or item == '..'
-    expect(page).to_not have_xpath("//img[@src=\"/assets/images/#{event_name}/#{item}\"]")
+    expect(page).to_not have_xpath("//img[@src=\"/assets/#{id_num}/medium/#{item}.*\"]")
   end
 end
