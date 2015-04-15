691e36cd762d12a63834a03058943fc7aa2c718a
Michael Soohoo <mbs729@gmail.com>
2015-04-14 21:14:28 -0700

    form stuff

--- app/assets/javascripts/new_edit_panel.js.jsx
+++ app/assets/javascripts/new_edit_panel.js.jsx
@@ -1,3 +1,6 @@
+var form = function() {
+}
+
 var newPanel = React.createClass({
   handleSubmit: function() {
     var name = this.refs.name.getDOMNode().value.trim();
@@ -9,11 +12,20 @@ var newPanel = React.createClass({
     var zip = this.refs.zip.getDOMNode().value().trim();
     var country = this.refs.country.getDOMNode().value().trim();
     var description = this.refs.description.getDOMNode().value.trim();
+
     this.props.onEventSubmit({title: title, start: start});
+
+    this.refs.name.getDOMNode().value = '';
+    this.refs.start.getDOMNode().value = '';
+    this.refs.end.getDOMNode().value = '';
+    this.refs.address_1.getDOMNode().value = '';
+    this.refs.city.getDOMNode().value = '';
+    this.refs.state.getDOMNode().value = '';
+    this.refs.zip.getDOMNode().value = '';
+    this.refs.country.getDOMNode().value = '';
+    this.refs.description.getDOMNode().value = '';
     return false;
   },
-  render: function() {
-    return (
-    );
-  }
+
+  render: form
 });
