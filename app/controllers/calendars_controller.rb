b005394fc4a76caa046b2d2b562731a4e715c388
Mike Vascelli <m.vascelli@berkeley.edu>
2015-04-18 23:46:32 -0700

    modified meetup

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,28 +36,26 @@ class CalendarsController < ApplicationController
   end
 
   def show
-    # For the moment keep running this task at every page view.
-    # But later I should switch to a scheduler (the link is on the browser)
     begin
       @page = Nokogiri::HTML(open("http://www.natureinthecity.org/"))
     rescue Exception
       preprocess_for_bad_request
     end
     preprocess_header_footer
-    if flash[:notice].nil? # For the moment prevent all of this if a message came in
+
+    if flash[:notice].nil? # Prevent Meetup synchronization if have incoming message
       events = Event.make_events_local(Event.get_remote_events)
+      display_synchronization_result(events)
+    end
+  end
 
-      ## CHECK FOR DELETION and do if needed
-      # Pluck the list of meetup_ids from database and compare with list of meetup_ids in events
-      # The difference (literally subtraction) at this point (i.e. after creation of new ones... will be in need of deletion)
-
-      if events.nil?
-        flash.now.notice = "Could not pull events from Meetup"
-      elsif events.empty?
-        flash.now.notice = "The Calendar and Meetup are synched"
-      else
-        flash.now.notice = 'Successfully pulled events: ' + CalendarsController.get_event_info(events) + ' from Meetup'
-      end
+  def display_synchronization_result(events)
+    if events.nil?
+      flash.now.notice = "Could not pull events from Meetup"
+    elsif events.empty?
+      flash.now.notice = "The Calendar and Meetup are synched"
+    else
+      flash.now.notice = 'Successfully pulled events: ' + self.class.get_event_info(events) + ' from Meetup'
     end
   end
 
