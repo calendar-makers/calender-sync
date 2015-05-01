ddf70b688581210c0a0a3f1e14bf5969804d5376
rachelsrabbit <rachelsrabbit@gmail.com>
2015-04-30 20:53:30 -0700

    don't click submit rsvp

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -5,17 +5,16 @@ class GuestsController < ActionController::Base
       return redirect_to event_path(@event.id), notice: 'Please fill out all fields to RSVP.'
     end
     handle_guest_registration
+    respond_to do |format|
+      format.html {render nothing: true}
+      format.json {render nothing: true}
+    end
   end
 
   def handle_guest_registration
     @guest = Guest.find_by_email(guest_params[:email]) || Guest.create!(guest_params)
-    if @guest.events.include?(@event)
-      redirect_to event_path(@event.id), notice: "#{@guest.email} is already registered for this event!"
-    else
-      @guest.registrations.build({:event_id => params[:event_id], :guest_id => @guest.id})
-      @guest.save
-      redirect_to event_path(@event.id), notice: "You successfully registered for this event!"
-    end
+    @guest.registrations.build({:event_id => params[:event_id], :guest_id => @guest.id})
+    @guest.save
   end
 
   private
