d1b55bf03ca7e92e302529f0971bfef78a7607c5
vincent <vincehayashi@yahoo.com>
2015-05-01 19:34:24 -0700

    switched to throwaway email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 '587',
-      user_name:            ENV[GMAIL_USERNAME],
-      password:             ENV[GMAIL_PASSWORD],
+      user_name:            'csynchronization@gmail.com',
+      password:             'natureinthecity',
       authentication:       'plain',
       enable_starttls_auto: true}
   
