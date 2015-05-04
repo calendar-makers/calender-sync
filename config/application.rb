51c99295541b16c1097c1ee0acb99794018bd6cb
Michael Soohoo <mbs729@gmail.com>
2015-05-04 01:57:53 -0700

    Precompile more for production.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module CalendarSync
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
   end
 end
