e54b79dbc5596b443745b377dfe501677bf40bb3
Michael Soohoo <mbs729@gmail.com>
2015-05-04 22:08:21 -0700

    Image tests pass.

--- features/step_definitions/images_steps.rb
+++ features/step_definitions/images_steps.rb
@@ -8,5 +8,5 @@ Then(/^I should see the picture "(.*)" for "(.*)"$/) do |image_name, event_name|
   sleep(2)
   step %{I click on "#{event_name}" in the calendar}
   id_num = Event.find_by_name(event_name).id
-  expect(page).to have_xpath("//img[starts-with(@src, '/assets/#{id_num}/medium/#{image_name}')]")
+  expect(page).to have_xpath("//img[starts-with(@src, '/assets/#{id_num}/original/#{image_name}')]")
 end
