3cc52fbed9ffa65df5d143b8c77444c1375a03b1
vincent <vincehayashi@yahoo.com>
2015-05-07 16:01:17 -0700

    added env variables, code will not work without email env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,10 +18,10 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
+      address:              'smtp.1and1.com',
       port:                 '587',
-      user_name:            'csynchronization@gmail.com',
-      password:             'natureinthecity',
+      user_name:            ENV['GMAIL_USERNAME'],
+      password:             ENV['GMAIL_PASSWORD'],
       authentication:       'plain',
       enable_starttls_auto: true}
   
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,10 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.1and1.com',
     port:                 '587',
-    user_name:            'csynchronization@gmail.com',
-    password:             'natureinthecity',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true}
 
