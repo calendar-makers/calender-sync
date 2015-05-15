a472b2d0a07675fd5974d2add098d170c565408f
Michael Soohoo <mbs729@gmail.com>
2015-05-14 20:55:08 -0700

    Set Rails timezone to US Pacific Time.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module CalendarSync
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.time_zone = 'Pacific Time (US & Canada)'
+    config.active_record.default_timezone = :local
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
