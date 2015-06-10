eb38531f88e15968fc16d902f7f887bad95b19ee
Mike Vascelli <m.vascelli@berkeley.edu>
2015-06-10 11:53:23 -0700

    switched to meetup data

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -7,14 +7,16 @@ class Meetup
   API_KEY = ENV['MEETUP_API']
 
   # NATURE IN THE CITY DATA
-=begin
   GROUP_ID = '8870202'
   GROUP_URLNAME = 'Nature-in-the-City'
   GROUP_NAME = 'Nature in the City'
-=end
+
+  # TESTING DATA
+=begin
   GROUP_ID = '1556336'
   GROUP_URLNAME = 'Meetup-API-Testing'
   GROUP_NAME = 'Meetup API Testing Sandbox'
+=end
 
   def default_group_name=(name='')
     @default_group_name = name.blank? ? GROUP_NAME : name
