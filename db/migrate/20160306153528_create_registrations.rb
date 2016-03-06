f2eb088356328d45960c9b206047c925e440f432
Mitchell Oleson <moleson21@berkeley.edu>
2016-03-06 23:51:28 +0000

    compressed database migrations

---
+++ db/migrate/20160306153528_create_registrations.rb
@@ -0,0 +1,13 @@
+class CreateRegistrations < ActiveRecord::Migration
+  def up
+    create_table :registrations do |t|
+      t.integer :event_id
+      t.integer :guest_id
+      t.timestamps
+    end
+  end
+  
+  def down
+    drop_table :registrations
+  end
+end
