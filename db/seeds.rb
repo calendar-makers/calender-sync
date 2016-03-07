d90761998d7549d572b276a46c54ce5fc06024a2
Mitchell Oleson <moleson21@berkeley.edu>
2016-03-07 00:51:56 +0000

    Added database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,30 @@
 
 events = [{:name => 'Market Street Prototyping Festival', :organization => 'Nature in the City', :description => 'A walk through the city', :venue_name => 'The Old Town Hall', :st_number => 145, :st_name => 'Jackson st', :city => 'Glendale', :zip => 90210, :start => 'April 09 2016 11:00', :end => 'March 19 2016, 20:30', :how_to_find_us => 'First door on left', :meetup_id => 122121212, :status => 'approved'},
           {:name => 'Nerds on Safari: Market Street', :organization => 'Green Carrots', :description => 'If you like beans youll like this event!', :venue_name => 'San Francisco City Library', :st_number => 45, :st_name => 'Seneca st', :city => 'Phoenix', :zip => 91210, :start => 'April 11 2016 00:00',:end => 'April 21 2016, 8:30', :how_to_find_us => 'Second door on the left', :meetup_id => 656555555, :status => 'approved'},
-      ]
+  ]
+
+guests = [{:first_name => 'Bob', :last_name => 'Richard', :phone => '(851) 345-0987', :email => 'brichard@gmail.com', :address => '234 Greensburg', :is_anon => False},
+  ]
+  
+registrations = [{:event_id => 1, :guest_id => 1},
+  ]
+
+users = [{:email => 'brichard@gmail.com', :encrypted_password => '', :reset_password_token => '', :reset_password_sent_at => 'January 11 2016 12:00', :failed_attempts => 0, :level => 0, :locked_at => ''},
+  ]
 
 events.each do |event|
   Event.create!(event)
+end
+
+guests.each do |guest|
+  Guest.create!(guest)
+end
+
+
+registrations.each do |registration|
+  Registrations.create!(registration)
+end
+
+users.each do |user|
+  Users.create!(user)
 end
