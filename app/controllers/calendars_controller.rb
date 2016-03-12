4c67dd5f56d36e31e97680daf7e140777f7ba729
Xin Yu Tan <xinyutan@berkeley.edu>
2016-03-12 07:07:06 +0000

    undo useless changes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,5 @@
 class CalendarsController < ApplicationController
-  
+
   before_filter do
     if request.ssl? && Rails.env.production?
       redirect_to :protocol => 'http://', :status => :moved_permanently
