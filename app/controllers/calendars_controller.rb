dd51b329e01e61470f8887ecb6e2191c02da6c4d
Ariel Swingley <ariel.swingley@berkeley.edu>
2016-03-12 06:44:04 +0000

    ready for merge into master

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,5 @@
 class CalendarsController < ApplicationController
-
+  
   before_filter do
     if request.ssl? && Rails.env.production?
       redirect_to :protocol => 'http://', :status => :moved_permanently
@@ -13,5 +13,6 @@ class CalendarsController < ApplicationController
     @upcoming = Event.where(:status => 'upcoming')
     @past = Event.where(:status => 'past')
     @rejected = Event.get_rejected_events
+    @head, @body = WebScraper.instance.page_data
   end
 end
