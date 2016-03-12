340fe7cbdbd630fa06f13e7b76e0bc0d5eaff50f
Ariel Swingley <ariel.swingley@berkeley.edu>
2016-03-12 04:24:35 +0000

    working

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,19 +7,12 @@ class CalendarsController < ApplicationController
   end
 
   def show
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     @tabs = %w(Upcoming Pending Rejected Past)
     @pending_count = Event.get_pending_events.count
     @pending = Event.get_pending_events
     @upcoming = Event.where(:status => 'upcoming')
     @past = Event.where(:status => 'past')
     @rejected = Event.get_rejected_events
->>>>>>> stuff
-=======
-    @pending = 1
->>>>>>> fdbd89ca5fe448b4cc11730e7a9c734c34318f36
     @head, @body = WebScraper.instance.page_data
   end
 end
